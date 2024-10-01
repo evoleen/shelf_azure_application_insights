b11ca559e15eb668fdaf04a26036c9b44ab807c4
Till Gerken <till.gerken@gmail.com>
2024-10-01 09:35:04 +0200

    first commit

---
+++ example/shelf_azure_application_insights_example.dart
@@ -0,0 +1,6 @@
+import 'package:shelf_azure_application_insights/shelf_azure_application_insights.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/shelf_azure_application_insights.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/shelf_azure_application_insights_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
