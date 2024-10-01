cff7d2fb261e9fbf2c6ef37e7e39604433d4cb61
Till Gerken <till.gerken@gmail.com>
2024-10-01 11:04:01 +0200

    Update example

--- example/shelf_azure_application_insights_example.dart
+++ example/shelf_azure_application_insights_example.dart
@@ -1,6 +1,20 @@
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_azure_application_insights/shelf_azure_application_insights.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+void main() async {
+  var handler = const Pipeline()
+      .addMiddleware(azureApplicationInsightsMiddleware(
+          connectionString: '<APPLICATION_INSIGHTS_CONNECTION_STRING>'))
+      .addHandler(_echoRequest);
+
+  var server = await shelf_io.serve(handler, 'localhost', 8080);
+
+  // Enable content compression
+  server.autoCompress = true;
+
+  print('Serving at http://${server.address.host}:${server.port}');
 }
+
+Response _echoRequest(Request request) =>
+    Response.ok('Request for "${request.url}"');
